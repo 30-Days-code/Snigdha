5943f16cb06e268384b2eab2b48ff0cc73d633a3
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-28 06:07:56 +1300

    added a getdata function

--- Question1.cpp
+++ Question1.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 class Book
 {
-    char n[20],a[20],p[20];
-    int n,p,c,ci; 
+    char N[100];
+    char A[100];
+    char P[100];
+    int n,p,t,ci; 
     public:
         void getdata();
         void availability();
         void return();
         void display
 };
-void Book:: getdata()
+void Book :: getdata()
 {
-     cout<<"Enter the name of the Book and Author name";
-     cin>>n>>p;
+    cout<<"Enter the name of the Book and Author name";
+    cin>>N>>A;
+    cout<<"Enter the name of publisher";
+    cin>>P;
+    cout<<"Enter the price and book no.";
+    cin>>p>>n;
+    cout<<"Enter the no. of total copies and issued copies";
+    cin>>tc>>ci;
 }
+void Book :: availability()
+{
+    
      
