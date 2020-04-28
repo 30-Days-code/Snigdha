045efa83d2dbb05a936555192153f8ac1650d8d8
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-29 06:02:03 +1300

    Create Question2.cpp

---
+++ Question2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class Employee
+{ 
+    int en;
+    char EN[100];
+    char Ad[100];
+    char D[100];
+};
+class Manager
+{
+    class Employee ::
+    int n;
+}
+int main()
+{
+  
+}
+
+
+    
