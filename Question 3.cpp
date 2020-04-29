eb19a01012a9f113f86a91ec78082003eea18b32
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-04-30 06:10:39 +1300

    written counting till 33

---
+++ Question 3.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"enter the no.";
+  cin>>n;
+  if(n==1){
+    cout<<"One";
+   }
+   else{
+    if(n==2){
+      cout<<"Two";
+    }
+    else
+    {
+      if(n==3){
+        cout<<"Three";
+       }
+       else{
+        if(n==4){
+          cout<<"Four";
+         }
+         if(n==5)
+         {
+            cout<<"Five";
+         }
+         else
+         {
+            if(n==6)
+            {
+              cout<<"Six";
+             }
+             else
+             {
+                if(n==7)
+                {
+                    cout<<"Seven";
+                }
+                else{
+                  if(n==8)
+                  {
+                      cout<<"Eight";
+                  }
+                  else{
+                      if(n==9){
+                        cout<<"Nine";
+                       }
+                       else{
+                          if(n==10){
+                            cout<<"Ten";
+                           }
+                           else
+                           {
+                              if(n==11)
+                              {
+                                cout<<"Eleven";
+                              }
+                              else{
+                                  if(n==12)
+                                  {
+                                    cout<<"Twelve";
+                                   }
+                                   else
+                                   {
+                                    if(n==13){
+                                      cout<<"Thirteen";
+                                    }
+                                    else
+                                    {
+                                      if(n==14){
+                                        cout<<"Fourteen";
+                                       }
+                                       else{
+                                        if(n==15){
+                                          cout<"Fifteen";
+                                         }
+                                         else{
+                                            if(n==16){
+                                              cout<<"Sixteen";
+                                             }
+                                             else{
+                                                if(n==17){
+                                                  cout<<"Seventeen";
+                                                 }
+                                                 else{
+                                                    if(n==18){
+                                                      cout<<"Eighteen";
+                                                     }
+                                                     else{
+                                                      if(n==19){
+                                                        cout<<"Nineteen";
+                                                       }
+                                                       else{
+                                                        if(n==20){
+                                                          cout<<"Twenty";
+                                                         }
+                                                         else{
+                                                          if(n==30){
+                                                            cout<<"Thirty";
+                                                            }
+                                                            else{
+                                                               if(n==31){
+                                                                cout<<"Thirty One";
+                                                               }
+                                                               else{
+                                                                if(n==32){
+                                                                  cout<<"Thirty Two";
+                                                                 }
+                                                                 else{
+                                                                  if(n==33){
+                                                                  
+                               
+                   
+        
+     
