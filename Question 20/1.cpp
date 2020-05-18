2e2bab01b300b1ec4517e718b8a4225d5c112a02
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-18 20:12:14 +1300

    Roots of the equation

---
+++ Question 20/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+  int a,b,c,d,x1,x2;
+  cout<<"Enter the values of a,b,c";
+  cin>>a>>b>>c;
+  cout<<a<<"x^2"<<"-"<<b<<"x"<<"+"<<c<<"= 0";
+  d=(b*b-4*a*c);
+  x1=(-b+sqrt(d))/2a;
+  x2=(-b-sqrtd))/2a;
+  cout<<"Roots are "<<x1<<x2;
+  return 0;
+ }
+ 
+  
+  
