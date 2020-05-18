d64601f38e44ba5a4cff8558b63b8dd692be2924
SnigdhaMarkanday <59870580+SnigdhaMarkanday@users.noreply.github.com>
2020-05-18 19:55:10 +1300

    Deleting an element from Array

--- Question 19.cpp
+++ Question 19.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+  int A[5]={3,4,5,6,7},d,f;
+  cout<<"Enter the element to be deleted";
+  cin>>d;
+  for(int i=0;i<5;i++){
+    if(d==A[i]){
+      cout<<"Found";
+      return 0;
+    }
+    else{
+      cout<<Not Found";
+    }
+  }
+  return 0;
+}
+  
+    
+   
